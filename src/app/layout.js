488a9f3fd6164a5adc4d1e7527e4e5eac191d6b3
Abhyudaya <kalpanaabhi764@gmail.com>
2026-01-18 00:55:23 +0530

    05 Ai model selector

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { QueryProvider } from "@/components/providers/query-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,7 +24,8 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <ThemeProvider
+        <QueryProvider>
+          <ThemeProvider
             attribute="class"
             defaultTheme="system"
             enableSystem
@@ -31,7 +33,7 @@ export default function RootLayout({ children }) {
           >
             {children}
           </ThemeProvider>
-  
+        </QueryProvider>
       </body>
     </html>
   );
